3a8a70b58ff2648d23f720d9c0de085a124316af
tprazniewski <104758997+tprazniewski@users.noreply.github.com>
2022-09-06 23:28:22 +0200

    Create validation validate: only  unique users

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -2,11 +2,13 @@ const User = require('../models/user')
 
 const create = async (req, res) => {
     const {name, email, password} = req.body;
-    
-    // new User({name: name, email: email, password: password})
+    const oldUser = await User.findOne({email});
+    // 401 status code for unathorised
+    if(oldUser) res.status(401).json({error: 'This email is already used'})
+    // new User({name: name, email: email, password: password}) // new User mus match the name of require variable
     const newUser = new User({name, email, password})
     await newUser.save()
-    res.json({user: newUser})
+    res.status(201).json({user: newUser})
 } 
 
 
--- backend/models/user.js
+++ backend/models/user.js
@@ -1,4 +1,5 @@
 const mongoose= require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = mongoose.Schema({
     name: {
@@ -17,5 +18,15 @@ const userSchema = mongoose.Schema({
         required: true,
     }
 })
+
+//When ever We save this file, before We save this file , we want to run function
+userSchema.pre('save', async function(next){
+    if (this.isModified('password')) {
+      const hash = await bcrypt.hash(this.password, 10)  
+      this.password = hash;
+    }
+    
+    next();
+} )
 
 module.exports = mongoose.model("User", userSchema)
