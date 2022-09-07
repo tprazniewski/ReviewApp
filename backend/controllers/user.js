a6b1b35a98b4d9d23c57e8f59621e7065973c9f1
tprazniewski <104758997+tprazniewski@users.noreply.github.com>
2022-09-07 10:57:02 +0200

    create OTP

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,4 +1,6 @@
 const User = require('../models/user')
+const emailVerificationToken = require('../models/emailVerificationToken')
+const nodemailer = require('nodemailer')
 
 const create = async (req, res) => {
     const {name, email, password} = req.body;
@@ -9,17 +11,37 @@ const create = async (req, res) => {
     const newUser = new User({name, email, password})
     await newUser.save()
 
+    //Generate OTP
+    let OTP = '';
+    for(let i = 0 ; i <=5; i++) {
+        const randomVal = Math.round(Math.random() * 9)
+        OTP += randomVal;
+    }
+    const newEmailVerificationToken = new emailVerificationToken({ owner: newUser._id, token: OTP})
+  
+    await newEmailVerificationToken.save();
 
-    // var transport = nodemailer.createTransport({
-    //     host: "smtp.mailtrap.io",
-    //     port: 2525,
-    //     auth: {
-    //       user: "be2db2821ba2ca",
-    //       pass: "532177fb48d064"
-    //     }
-    //   });
-
-    res.status(201).json({user: newUser})
+      // Store OPT inside db
+    // send that otp to our user
+    var transport = nodemailer.createTransport({
+        host: "smtp.mailtrap.io",
+        port: 2525,
+        auth: {
+          user: "be2db2821ba2ca",
+          pass: "532177fb48d064"
+        }
+      });
+    
+      transport.sendMail({
+        from: ' verification',
+        to: newUser.email,
+        subject: 'Email Verification',
+        html: `
+        <p> Your OTP </p>
+        <h1> ${OTP} </h1>
+        `
+      })
+    res.status(201).json({message: "please verify your email. OTP has been sent"})
 } 
 
 
