cf19b1035530683f3465e5274c973218c463af81
tprazniewski <104758997+tprazniewski@users.noreply.github.com>
2022-10-12 19:24:24 +0200

    Wrap Login and img with the Link to have navigation inside the app

--- frontend/src/components/user/Navbar.jsx
+++ frontend/src/components/user/Navbar.jsx
@@ -1,27 +1,36 @@
-import React from 'react'
-import {BsEmojiSunglasses} from 'react-icons/bs'
-import Container from '../Container'
+import React from "react";
+import { BsEmojiSunglasses } from "react-icons/bs";
+import { Link } from "react-router-dom";
+import Container from "../Container";
 
 export default function Navbar() {
   return (
     <div className="bg-secondary shadow-sm shadow-gray-500">
-        <Container className="p-2"> 
-            <div className='flex justify-between items-center'>
-                <img src="./logo.png" alt="logo Img" className='h-10'/>
-                <ul className='flex items-center space-x-2'>
-                    <li>
-                        <button className='bg-dark-subtle p-1 rounded'>
-                            <BsEmojiSunglasses className='text-secondary' size={24}/>
-                        </button>
-                    </li>
-                    <li>
-                        <input type="text" className='border-2 border-dark-subtleborder-2 p-1 rounded bg-transparent text-lg focus:border-white transition text-white' placeholder='Search'/>
-                    </li>
-                    <li className='text-white font-semibold text-lg'>Login</li>
-                </ul>
-            </div>
-        </Container>
+      <Container className="p-2">
+        <div className="flex justify-between items-center">
+          <Link to='/'>
+            <img src="./logo.png" alt="logo Img" className="h-10" />
+          </Link>
+          <ul className="flex items-center space-x-2">
+            <li> 
+              <button className="bg-dark-subtle p-1 rounded">
+                <BsEmojiSunglasses className="text-secondary" size={24} />
+              </button>
+            </li>
+            <li>
+              <input
+                type="text"
+                className="border-2 border-dark-subtleborder-2 p-1 rounded bg-transparent text-lg focus:border-white transition text-white"
+                placeholder="Search"
+              />
+            </li>
+            <li >
+            <Link className="text-white font-semibold text-lg" to='/auth/signin'>Login</Link>
+            
+            </li>
+          </ul>
+        </div>
+      </Container>
     </div>
-
-  )
+  );
 }
