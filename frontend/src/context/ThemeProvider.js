8648b0f40e69203df68110297b04f2ea80e46d70
tprazniewski <104758997+tprazniewski@users.noreply.github.com>
2022-10-13 10:27:45 +0200

    Create toggle dark/light theme functionality

--- frontend/src/context/ThemeProvider.js
+++ frontend/src/context/ThemeProvider.js
@@ -5,16 +5,23 @@ export const ThemeContext = createContext();
 
 
 export default function ThemeProvider({ children }) {
+  const defaultTheme = 'light'
+  const darkTheme = 'dark'
 
   const toggleTheme = () => {
-    document.documentElement.classList.add('dark')
+    const oldTheme = localStorage.getItem('theme')
+    const newTheme = oldTheme === defaultTheme ? darkTheme : defaultTheme
+    document.documentElement.classList.add(newTheme)
+    document.documentElement.classList.remove(oldTheme)
     // console.log(document.documentElement)
-    localStorage.setItem('theme', 'dark')
+    localStorage.setItem('theme', newTheme)
   }
 
   useEffect(()=> {
+    
     const theme = localStorage.getItem('theme')
-    document.documentElement.classList.add(theme)
+    if(!theme) document.documentElement.classList.add(defaultTheme)
+    else document.documentElement.classList.add(theme)
   },[])
 
   return( 
