adc673edbc9fc16a470ffc3b55d65a8708167aca
tprazniewski <104758997+tprazniewski@users.noreply.github.com>
2022-10-14 09:18:58 +0200

    Create handlers and keep working on signUp form

--- frontend/src/components/auth/Signup.jsx
+++ frontend/src/components/auth/Signup.jsx
@@ -1,32 +1,72 @@
-import React from 'react'
-import Container from '../Container'
-import Title from '../form/Title'
-import FormInput from '../form/FormInput'
-import Submit from '../form/Submit'
-import CustomLink from '../CustomLink'
-import { commonModalClasses } from '../../utils/theme'
-import FormContainer from '../form/FormContainer'
+import React, { useState } from "react";
+import Container from "../Container";
+import Title from "../form/Title";
+import FormInput from "../form/FormInput";
+import Submit from "../form/Submit";
+import CustomLink from "../CustomLink";
+import { commonModalClasses } from "../../utils/theme";
+import FormContainer from "../form/FormContainer";
 
 export default function Signup() {
+  const [userInfo, setUserInfo] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const { name, email, password } = userInfo;
+
+  // const handleChange = (e)=>{
+  //   e.target
+  // }
+  const handleChange = ({target})=>{
+    // console.log(target.value, target.name)
+    const {value,name} = target
+    setUserInfo({...userInfo, [name]: value})
+  }
+
+  const handleSubmit = (e)=> {
+    e.preventDefault()
+    console.log(userInfo)
+  }
+
   return (
     // <div className='fixed inset-0 dark:bg-primary bg-white -z-10 flex justify-center items-center'>
-      <FormContainer>
-        <Container>
-            <form className= {commonModalClasses + ' w-72'}>
-                <Title> Sign up</Title>
-                <FormInput label='Name' placeholder='Mike Sanders' name='name'/>
-                <FormInput label='Email' placeholder='Sanders@email.com' name='email'/>
-                <FormInput label='Password' placeholder='********' name='password'/>
-                <Submit value ='Sign up'/>
-                
-                <div className='flex justify-between'>
-                    <CustomLink to='/auth/forget-password'> ForgetPassword</CustomLink>
-                    <CustomLink to='/auth/signin'>Sign in</CustomLink>
+    <FormContainer>
+      <Container>
+        <form onSubmit={handleSubmit} className={commonModalClasses + " w-72"}>
+          <Title> Sign up</Title>
+          <FormInput
+            onChange={handleChange}
+            value={name}
+            label="Name"
+            placeholder="Mike Sanders"
+            name="name"
+          />
+          <FormInput
+            onChange={handleChange}
+            value={email}
+            label="Email"
+            placeholder="Sanders@email.com"
+            name="email"
+          />
+          <FormInput
+            onChange={handleChange}
+            value={password}
+            type="password"
+            label="Password"
+            placeholder="********"
+            name="password"
+          />
+          <Submit value="Sign up" />
 
-                </div>
-            </form> 
-        </Container>
-      </FormContainer>
+          <div className="flex justify-between">
+            <CustomLink to="/auth/forget-password"> ForgetPassword</CustomLink>
+            <CustomLink to="/auth/signin">Sign in</CustomLink>
+          </div>
+        </form>
+      </Container>
+    </FormContainer>
     // </div>
-  )
+  );
 }
--- frontend/src/components/form/FormInput.jsx
+++ frontend/src/components/form/FormInput.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
 
-export default function FormInput({name, label, placeholder, ...rest}) {
+export default function FormInput({ name, label, placeholder, ...rest }) {
   return (
-    <div className='flex flex-col-reverse'>
-        <input id={name} name ={name} className='bg-transparent rounded border-2 dark:border-dark-subtle border-light-subtle w-full text-lg outline-none dark:focus:border-white p-1 focus:border-primary dark:text-white peer transition' placeholder={placeholder} {...rest}/>
-        <label htmlFor={name} className='font-semibold dark:text-dark-subtle text-light-subtle dark:peer-focus:text-white peek-focus:text-primary transition self-start ' >{label}</label>
-    </div>  )
+    <div className="flex flex-col-reverse">
+      <input
+        id={name}
+        name={name}
+        className="bg-transparent rounded border-2 dark:border-dark-subtle border-light-subtle w-full text-lg outline-none dark:focus:border-white p-1 focus:border-primary dark:text-white peer transition"
+        placeholder={placeholder}
+        {...rest}
+      />
+      <label
+        htmlFor={name}
+        className="font-semibold dark:text-dark-subtle text-light-subtle dark:peer-focus:text-white peek-focus:text-primary transition self-start "
+      >
+        {label}
+      </label>
+    </div>
+  );
 }
