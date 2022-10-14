d529a71c930daf6abf7ce9214bba3f43748cf018
tprazniewski <104758997+tprazniewski@users.noreply.github.com>
2022-10-14 10:07:41 +0200

    Fix cors error

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const userRouter = require('./routes/user')
+const cors = require('cors')
 const app = express();
 require('./db')
 
 //We need this middleware to parse chunk of data to a redable format
+app.use(cors())
 app.use(express.json())
 app.use('/api/user', userRouter)
 
